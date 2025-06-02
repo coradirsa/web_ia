381b545f3f6399fb1b80906d6fd4fcded49975e6
ValentinGarro <vgarro@coradir.com.ar>
2025-06-02 14:48:52 -0300

    limpiando el proyecto

---
+++ src/hooks/useMediaQuery.jsx
@@ -0,0 +1,21 @@
+export const useMediaQuery = (query) => {
+    const [matches, setMatches] = useState(false);
+  
+    useEffect(() => {
+      const media = window.matchMedia(query);
+      
+      const updateMatch = (e) => {
+        setMatches(e.matches);
+      };
+      
+      setMatches(media.matches);
+      media.addEventListener('change', updateMatch);
+      
+      return () => {
+        media.removeEventListener('change', updateMatch);
+      };
+    }, [query]);
+  
+    return matches;
+  };
+  
