de157cc1e50e21eef8edc75d745b28e4477ea633
ValentinGarro <vgarro@coradir.com.ar>
2025-06-03 12:22:17 -0300

    header y navbar responsive finalizado

--- src/app/components/header/components/navLink.jsx
+++ src/app/components/header/components/navLink.jsx
@@ -1,11 +1,11 @@
 export default function NavLink({href, text , isBarrer}) {
     return (
-        <div className="relative flex items-center justify-center h-10 min-w-32 xl:min-w-36">
+        <div className="relative flex items-center justify-center h-10 w-full max-w-44 xl:max-w-40">
             <a 
                 href={href}
-                className="text-white font-bold uppercase text-xs md:text-lg cursor-pointer" 
+                className="text-white font-bold uppercase text-[10px] sm:text-xs xl:text-sm  cursor-pointer w-full sm:px-2 md:px-3 " 
             >{text}</a>
-           {isBarrer && <span className="absolute top-0 right-0 w-0.5 h-full bg-black"></span>} 
+           {isBarrer && <span className="absolute top-0 right-0 w-[1px] sm:w-[2px] h-full md:bg-gray "></span>} 
         </div>
     )
 }
