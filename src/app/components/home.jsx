519677f1f1283d48bf91260628fedbfd10031b0f
ValentinGarro <vgarro@coradir.com.ar>
2025-06-03 12:54:30 -0300

    seccion de productos responsive terminada

--- src/app/components/home.jsx
+++ src/app/components/home.jsx
@@ -2,9 +2,9 @@ import Button from "./button";
 
 export default function Home() {
     return (
-        <section id="home" className="w-full min-h-[100vh] -mt-28   md:pt-40 px-10
+        <section id="home" className="w-full min-h-[100vh] -mt-28  md:pt-40 px-10
             bg-[url('/img/01.webp')] bg-cover xs:bg-center bg-no-repeat 
-            flex justify-start items-center
+            flex justify-start items-center py-10
         ">
             <div className="flex flex-col gap-2 md:gap-5  w-full md:w-1/2">
                 <h2 className="text-white text-4xl sm:text-5xl md:text-7xl font-bold tracking-tighter uppercase" >Inteligencia artificial que impulsa el éxito</h2>
--- src/app/components/products/products.jsx
+++ src/app/components/products/products.jsx
@@ -3,19 +3,19 @@ import Button from "../button";
 
 export default function Products() {
     return(
-        <section id="products" className="flex flex-col justify-start items-center w-full  bg-white p-10 gap-4">
-            <h3 className="text-background text-5xl md:text-6xl font-bold tracking-tighter uppercase w-full">Explorá la ia conversacional</h3>
-            <h4 className="text-gray text-2xl md:text-3xl font-bold italic uppercase mb-2 w-full">Chatbot inteligente</h4>
-            <div className="flex flex-col items-center xl:flex-row justify-center md:justify-between w-full py-10">
+        <section id="products" className="flex flex-col justify-start items-center w-full  bg-white p-5 xs:p-10 xs:gap-4 py-15">
+            <h3 className="text-background text-3xl sm:text-5xl md:text-6xl font-bold tracking-tighter uppercase w-full">Explorá la ia conversacional</h3>
+            <h4 className="text-gray text-lg sm:text-xl md:text-3xl font-bold italic uppercase xs:mb-2 w-full">Chatbot inteligente</h4>
+            <div className="flex flex-col items-center xl:flex-row justify-center md:justify-between w-full py-5 xs:py-10">
                 <div className="md:1/3 xl:w-1/2 mb-10 xl:mb-0 flex flex-col justify-center ">
-                    <p className="text-black text-xl lg:text-2xl p-10 border-l-[1px] border-r-[1px] border-gray text-left rounded-3xl -mb-4 box-fade-vertical">
+                    <p className="text-black text-xs sm:text-xl lg:text-2xl px-3 py-10 xs:p-5 sm:p-10 border-l-[1px] border-r-[1px] border-gray text-left rounded-3xl -m-3 sm:-mb-4 box-fade-vertical">
                         Mejorá la comunicación con tus clientes de una forma más eficiente de compra y asegurá tu éxito.<br/>
                         Diseñá tu ChatBot acorde a tus necesidades y marcá la diferencia ahora.
                     </p>
                     <Button 
                         element="Quiero un chatbot" 
                         type="button" 
-                        className="relative bg-background rounded-full px-5 py-2 w-56 text-white h-10 ml-16 hover:bg-black hover:shadow-[0_0_15px_rgba(0,0,0,0.4)]"
+                        className="relative bg-background rounded-full px-1 sm:px-5 py-1 w-44 sm:w-56 text-white sm:h-10 ml-5 sm:ml-16 text-xs sm:text-sm hover:bg-black hover:shadow-[0_0_15px_rgba(0,0,0,0.4)]"
                     />
                 </div>
                 <Image 
